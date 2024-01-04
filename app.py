04b1b7e883c8d785ea2666c9736ae9097edbd635
Pranav <pranavww@gmail.com>
2024-01-04 23:45:49 +0530

    Updated input page

--- app.py
+++ app.py
@@ -4,13 +4,26 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pickle
 import json
-import csv
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 app = Flask(__name__)
 
 symptom_list = ['Itching', 'Skin Rash', 'Nodal Skin Eruptions', 'Continuous Sneezing', 'Shivering', 'Chills', 'Joint Pain', 'Stomach Pain', 'Acidity', 'Ulcers On Tongue', 'Muscle Wasting', 'Vomiting', 'Burning Micturition', 'Spotting Urination', 'Fatigue', 'Weight Gain', 'Anxiety', 'Cold Hands And Feets', 'Mood Swings', 'Weight Loss', 'Restlessness', 'Lethargy', 'Patches In Throat', 'Irregular Sugar Level', 'Cough', 'High Fever', 'Sunken Eyes', 'Breathlessness', 'Sweating', 'Dehydration', 'Indigestion', 'Headache', 'Yellowish Skin', 'Dark Urine', 'Nausea', 'Loss Of Appetite', 'Pain Behind The Eyes', 'Back Pain', 'Constipation', 'Abdominal Pain', 'Diarrhoea', 'Mild Fever', 'Yellow Urine', 'Yellowing Of Eyes', 'Acute Liver Failure', 'Fluid Overload', 'Swelling Of Stomach', 'Swelled Lymph Nodes', 'Malaise', 'Blurred And Distorted Vision', 'Phlegm', 'Throat Irritation', 'Redness Of Eyes', 'Sinus Pressure', 'Runny Nose', 'Congestion', 'Chest Pain', 'Weakness In Limbs', 'Fast Heart Rate', 'Pain During Bowel Movements', 'Pain In Anal Region', 'Bloody Stool', 'Irritation In Anus', 'Neck Pain', 'Dizziness', 'Cramps', 'Bruising', 'Obesity', 'Swollen Legs', 'Swollen Blood Vessels', 'Puffy Face And Eyes', 'Enlarged Thyroid', 'Brittle Nails', 'Swollen Extremeties', 'Excessive Hunger', 'Extra Marital Contacts', 'Drying And Tingling Lips', 'Slurred Speech', 'Knee Pain', 'Hip Joint Pain', 'Muscle Weakness', 'Stiff Neck', 'Swelling Joints', 'Movement Stiffness', 'Spinning Movements', 'Loss Of Balance', 'Unsteadiness', 'Weakness Of One Body Side', 'Loss Of Smell', 'Bladder Discomfort', 'Foul Smell Of Urine', 'Continuous Feel Of Urine', 'Passage Of Gases', 'Internal Itching', 'Toxic Look (Typhos)', 'Depression', 'Irritability', 'Muscle Pain', 'Altered Sensorium', 'Red Spots Over Body', 'Belly Pain', 'Abnormal Menstruation', 'Dischromic Patches', 'Watering From Eyes', 'Increased Appetite', 'Polyuria', 'Family History', 'Mucoid Sputum', 'Rusty Sputum', 'Lack Of Concentration', 'Visual Disturbances', 'Receiving Blood Transfusion', 'Receiving Unsterile Injections', 'Coma', 'Stomach Bleeding', 'Distention Of Abdomen', 'History Of Alcohol Consumption', 'Fluid Overload', 'Blood In Sputum', 'Prominent Veins On Calf', 'Palpitations', 'Painful Walking', 'Pus Filled Pimples', 'Blackheads', 'Scurring', 'Skin Peeling', 'Silver Like Dusting', 'Small Dents In Nails', 'Inflammatory Nails', 'Blister', 'Red Sore Around Nose', 'Yellow Crust Ooze', 'Prognosis']
-loaded_model = pickle.load(open('Model_Files/trained_model.sav','rb'))
+
+sympto_model = pickle.load(open('Model_Files/trained_model.sav','rb'))
 label_encoder = pickle.load(open('Model_Files/label_encoder_target.sav','rb'))
+
+mental_model = pickle.load(open('Model_Files/stress_check.sav','rb'))
+vectorizer = pickle.load(open('Model_Files/Vectorizer.sav','rb'))
 
 data_file_path = 'Model_Files/data.csv'
 file_path = 'Model_Files/disease_data.json'
@@ -18,6 +31,15 @@ file_path = 'Model_Files/disease_data.json'
 with open(file_path, 'r') as file:
     disease_info = json.load(file)
 
+def remove_stopwords(text):
+    stpw=set(stopwords.words('english'))
+    filtered_text=[word for word in text if word not in stpw]
+    return filtered_text
+
+def lemmatize_words(text):
+    lemmer=WordNetLemmatizer()
+    lemmatized_text=[lemmer.lemmatize(word,pos='v') for word in text]
+    return lemmatized_text
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -54,7 +76,7 @@ def predict():
                 input_vector[symptom_list.index(i)] = 1
         inp = input_vector.reshape(1,-1)
 
-        probabilities = loaded_model.predict_proba(inp)
+        probabilities = sympto_model.predict_proba(inp)
 
         top_n = 5
         top_classes = np.argsort(probabilities[0])[::-1][:top_n]  # Indices of the top N classes
@@ -70,7 +92,29 @@ def predict():
         return render_template('symptomOutput.html', prediction=list(possiblities), info = pred_info)
     
     return render_template('symptomInput.html')
+
+@app.route('/mental', methods=['GET', 'POST'])
+def mental():
+    if request.method == "POST":
+        input = request.form['feelings']
+        input=pd.DataFrame({
+            'text':[input]
+        })
+        input['text']=input['text'].apply(word_tokenize)
+        input['text']=input['text'].apply(remove_stopwords)
+        input['text']=input['text'].apply(lemmatize_words)
+        input['text']=input['text'].apply(lambda x : ' '.join([index for index in x]))
+        X_valid=vectorizer.transform(input['text'])
+        stress_output=mental_model.predict(X_valid)
+        stress_probab = mental_model.predict_proba(X_valid)[1]
+        if stress_output==0:
+            stress_output='Not Stress'
+        else:
+            stress_output='Stress'
         
+        print(stress_output)
+
+    return render_template('mentalhealth.html')        
 
 @app.route('/output')
 def predictions():
