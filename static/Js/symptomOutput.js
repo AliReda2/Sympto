d156c324a622679d5f9d198d254d23cf86c08b09
AliReda2 <alireda2572003@gmail.com>
2024-05-24 17:19:13 +0300

    gg

--- static/Js/symptomOutput.js
+++ static/Js/symptomOutput.js
@@ -15,22 +15,3 @@ function toggleInfo(index) {
         }
     });
 }
-
-function getRandomQuote() {
-      var category = 'happiness';
-      $.ajax({
-          method: 'GET',
-          url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-          headers: { 'X-Api-Key': 'T5gjwYT90I24ZrPU5nUBbA==wbNu9T8q7bv0VJ6T' },
-          contentType: 'application/json',
-          success: function (result) {
-              // Append the quote to the quoteContainer div
-              $('#quoteContainer').html('<p>' + '"' + result[0].quote + '"' + '</p>');
-          },
-          error: function ajaxError(jqXHR) {
-              console.error('Error: ', jqXHR.responseText);
-          }
-      });
-  }
-  toggleInfo(0)
-getRandomQuote();
